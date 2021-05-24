6ff86008a1f3fb88a3c1f3a937f7e1996dc09413
Illarion Koperski <illarionvk@gmail.com>
2021-05-24 13:17:48 +0200

    Add Nix packages

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -9,6 +9,20 @@
   home.username = "hex";
   home.homeDirectory = "/Users/hex";
 
+  home.packages = [
+    # pkgs is the set of all packages in the default home.nix implementation
+    pkgs.ag
+    pkgs.fzf
+    pkgs.git
+    pkgs.neovim
+    pkgs.python3
+    pkgs.ripgrep
+    pkgs.ruby
+    pkgs.tmux
+    pkgs.vim
+    pkgs.wget
+  ];
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
