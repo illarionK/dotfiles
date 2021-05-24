6ea4c446460cf72b468700746195e893b5503699
Illarion Koperski <illarionvk@gmail.com>
2021-05-24 14:42:33 +0200

    Install Python with Pip

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1,5 +1,13 @@
 { config, pkgs, ... }:
 
+let
+
+  python-with-global-packages = pkgs.python3.withPackages(ps: with ps; [
+    pip
+  ]);
+
+in
+
 {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
@@ -14,13 +22,16 @@
     pkgs.ag
     pkgs.fzf
     pkgs.git
+    pkgs.htop
+    pkgs.jq
     pkgs.neovim
-    pkgs.python3
     pkgs.ripgrep
     pkgs.ruby
     pkgs.tmux
+    pkgs.tree
     pkgs.vim
     pkgs.wget
+    python-with-global-packages
   ];
 
   # This value determines the Home Manager release that your
